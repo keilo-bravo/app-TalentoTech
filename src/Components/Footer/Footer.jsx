088ef77d842e59458e046fc1e539aed75bbb3a26
keilo-bravo <kesbto@gmail.com>
2026-05-27 06:23:41 -0300

    mini fix

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     return <footer className="foo">
             <p className="foo1">© Changuchito Premium 2026</p>
             <p className="foo1">RRSS</p>
-            <p className="foo1">Desarrollado por YO</p>
+            <p className="foo1">Desarrollado por KeiloBravo</p>
         </footer>;
 }
 export default Footer;
